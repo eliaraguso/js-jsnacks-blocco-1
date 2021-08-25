d7733671ebe498664f21d1db7edeb59bb12655a5
eliaraguso <elia.raguso@gmail.com>
2021-08-25 15:51:58 +0200

    aggiunta la richiesta all'utente di inserire un numero per 6 volte

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+// chiedo all'utente di inserire  un  numero per 6 volte
+for (var i = 0; i < 6; i++) {
+    var numero = parseInt (prompt("Inserisci un numero"))  
+}
+
