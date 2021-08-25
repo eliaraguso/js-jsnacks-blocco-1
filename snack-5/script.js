ccf639d14adfcfab05ca93b28ebf3d46141e4fe0
eliaraguso <elia.raguso@gmail.com>
2021-08-25 15:55:14 +0200

    creato un array vuoto

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,5 +1,8 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 
+// Creo un array vuoto
+var contenitoreNumeri = []
+
 // chiedo all'utente di inserire  un  numero per 6 volte
 for (var i = 0; i < 6; i++) {
     var numero = parseInt (prompt("Inserisci un numero"))  
